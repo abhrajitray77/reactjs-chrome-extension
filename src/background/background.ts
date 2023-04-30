d33a044e951a21a1b63e8119615a26296d3a8de2
abhrajitray77 <abhrajitray77@gmail.com>
2023-04-30 10:50:04 +0530

    works || initial commit

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,5 +1,6 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    console.log(msg);
-    console.log(sender);
-    sendResponse("Front the background Script");
-})
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.commands.onCommand.addListener((command: string) => {
+      console.log(`command triggered is ${command}`)
+    })
+  })
+  
