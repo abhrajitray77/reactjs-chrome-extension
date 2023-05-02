c1b7f8315bde9ddf649a49b97fe3ba936f7e5fd3
abhrajitray77 <abhrajitray77@gmail.com>
2023-05-02 22:03:29 +0530

    added delete func

--- src/components/Clips.tsx
+++ src/components/Clips.tsx
@@ -1,3 +1,4 @@
+import { TrashIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import React, { useEffect, useState } from "react";
 
 const Clips = () => {
@@ -17,22 +18,52 @@ const Clips = () => {
     });
   }, []);
 
+  const handleClearStorage = () => {
+    chrome.storage.local.clear(() => {
+      setElements([]);
+    });
+  };
+
+  const handleRemoveItem = (id) => {
+    chrome.storage.local.remove(id, () => {
+      setElements((prevState) => prevState.filter((elem) => elem.id !== id));
+    });
+  };
+
   return (
     <div>
       <h1 className="text-gray-100 font-semibold text-xl">
         Here's your saved clips-
       </h1>
       {elements.length === 0 && (
-        <p className="text-gray-100">No clips saved yet!</p>
+        <p className="text-gray-100 mt-2">
+          Press Ctrl+Shift+Y to trigger the extension and click on an element to
+          save it!
+        </p>
       )}
-      <ul className="text-gray-100">
-        {elements.map((element) => (
-          <li key={element.id} className=" hover:bg-black/20 m-1">
-            <p>Text: {element.text}</p>
-            <p>URL: {element.url}</p>
-          </li>
-        ))}
-      </ul>
+      {elements.length > 0 && (
+        <div>
+          <button
+            className="absolute top-4 right-4 text-red-500 hover:text-red-600 bg-gray-200
+          w-6 h-6 p-1 hover:scale-105 transition-all duration-300 rounded-md cursor-pointer
+          "
+            onClick={handleClearStorage}
+          >
+            <TrashIcon />
+          </button>
+          <ul className="text-gray-100">
+            {elements.map((element) => (
+              <li key={element.id} className=" hover:bg-black/20 m-1 p-[2px] truncate">
+                <p>Text: {element.text}</p>
+                <p>URL: {element.url}</p>
+                <button onClick={() => handleRemoveItem(element.id)} className="text-red-500 w-6 h-6">
+                  <XMarkIcon />
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
